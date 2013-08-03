a61d5180ef35494810a29eb20c40ca852d208caf
Corey Quillen <corey.quillen@gmail.com>
2013-08-03 15:16:02 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mockups::Application.config.secret_key_base = '77167d8f024962362941989c85b4e86f93df074116faec1239718f5204e0d064e86e959b6d7b15753b2d6337294618c95dda0e97c7d8019c1ca46c7058d00adf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mockups::Application.config.session_store :cookie_store, key: '_mockups_session'
