cd675ade5500e887f333b238aa2f2884c000b687
Corey Quillen <corey.quillen@gmail.com>
2013-11-16 21:58:52 -0600

    create 3 column with sidebar planning layout concept

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,7 @@ class PagesController < ApplicationController
 	def scrum_board
 	end
 	
+	def sidebar
+	end
+	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Mockups::Application.routes.draw do
 
   get 'pages/scrum_board' => 'pages#scrum_board'
+  get 'pages/sidebar' => 'pages#sidebar'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
